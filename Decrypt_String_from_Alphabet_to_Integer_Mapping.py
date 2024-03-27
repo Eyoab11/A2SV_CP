68463cbefa6f61f81c2f941ae48db37b31de0e27
Eyoab11 <134016127+Eyoab11@users.noreply.github.com>
2024-03-27 23:44:51 +0300

    Create Decrypt_String_from_Alphabet_to_Integer_Mapping.py

---
+++ Decrypt_String_from_Alphabet_to_Integer_Mapping.py
@@ -0,0 +1,17 @@
+#1309. Decrypt String from Alphabet to Integer Mapping
+class Solution:
+    def freqAlphabets(self, s: str) -> str:
+        result = ""
+        i = len(s) - 1
+        while i >= 0:
+            if s[i] == '#':
+                number = int(s[i - 2:i])
+                result += chr(number + ord('a') - 1)
+                i -= 3
+            else:
+                number = int(s[i])
+                result += chr(number + ord('a') - 1)
+                i -= 1
+        return result[::-1]
+        
+        
