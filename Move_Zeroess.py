96f8d01f4b3d12d4368c22f12596c81e91b5e93b
Eyoab11 <134016127+Eyoab11@users.noreply.github.com>
2024-05-03 19:26:57 +0300

    Create Move_Zeroess.py

---
+++ Move_Zeroess.py
@@ -0,0 +1,11 @@
+# 283. Move Zeroes
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        i = 0
+        non_zero = 0
+        while i < len(nums):
+            if nums[i] != 0:
+                nums[i], nums[non_zero] = nums[non_zero], nums[i]
+                non_zero += 1
+            i += 1
+        return nums
