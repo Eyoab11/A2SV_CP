6d51d2b05eaae1c1a6d8d8f3ef18bc5b42763b73
Eyoab11 <134016127+Eyoab11@users.noreply.github.com>
2024-05-04 22:58:45 +0300

    Create MaxConsecutiveOnesIII.py

---
+++ MaxConsecutiveOnesIII.py
@@ -0,0 +1,15 @@
+#1004. Max Consecutive Ones III
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        left = 0
+        max_ = 0
+        i = 0
+        while i < len(nums):
+            n = nums[i]
+            k -= (1 - n)
+            if k < 0:
+                k += (1 - nums[left])
+                left += 1
+            max_ = max(max_, i - left + 1)
+            i += 1
+        return max_
