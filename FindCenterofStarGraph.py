391ba41c4606b182d106cf9f2403438a26cdd3f3
Eyoab11 <134016127+Eyoab11@users.noreply.github.com>
2024-08-13 18:54:49 +0300

    Create FindCenterofStarGraph.py

---
+++ FindCenterofStarGraph.py
@@ -0,0 +1,8 @@
+#1791. Find Center of Star Graph
+class Solution:
+    def findCenter(self, edges: List[List[int]]) -> int:
+        if edges[0][0] in edges[1]:
+            return edges[0] [0]
+        return edges[0] [1]
+
+        
